6ddd6e38531c7c550db8eafe6689b2b9e951050f
Aster <galaster@foxmail.com>
2020-10-16 14:22:11 +0800

    🔮 Implement square sampling

--- projects/vrellis-core/src/render/mod.rs
+++ projects/vrellis-core/src/render/mod.rs
@@ -5,10 +5,16 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum VrellisShape {
-    Circle { scale: f32 },
-    Triangle { scale: f32, rotate: f32 },
-    Square { scale: f32, rotate: f32 },
+    Circle,
+    Triangle,
+    Square,
+    Parabola,
+    /// Note that it must be a convex hull
     Polygon { edges: Vec<(u32, u32)> },
+    /// Note that it must be a convex curve
+    Custom{
+        points: Vec<VrellisPoint>
+    }
 }
 
 
@@ -46,20 +52,38 @@ impl Default for VrellisColorMode {
 }
 
 impl VrellisShape {
-    pub fn sample(&self, num: u32) -> Vec<VrellisPoint> {
+    pub fn sample(&self, num: u32,scale:f32) -> Vec<VrellisPoint> {
         assert!(num > 9, "too less samples!");
         let mut out = Vec::with_capacity(num as usize);
         match self {
-            VrellisShape::Circle { scale } => {
+            VrellisShape::Circle=> {
                 for n in 0..num {
                     let x = (scale / 2.0) * (n as f32).cos();
                     let y = (scale / 2.0) * (n as f32).sin();
                     out.push(VrellisPoint { n, x: x as u32, y: y as u32 })
                 }
             }
-            VrellisShape::Triangle { .. } => unimplemented!(),
-            VrellisShape::Square { .. } => unimplemented!(),
+            VrellisShape::Triangle => {
+                assert_eq!(num % 4, 0, "Must be a multiple of 3");
+                unimplemented!()
+            },
+            VrellisShape::Square => {
+                assert_eq!(num % 4, 0, "Must be a multiple of 4");
+                for n in 0..(num /4) {
+                    let w = scale as u32;
+                    out.push(VrellisPoint { n:0*num /4 +n, x: x as u32, y: 0 });
+                    out.push(VrellisPoint { n:1*num /4 +n, x: w, y: y as u32 });
+                    out.push(VrellisPoint { n:2*num /4 +n, x: x as u32, y: y as u32 });
+                    out.push(VrellisPoint { n:3*num /4 +n, x: x as u32, y: y as u32 })
+                }
+
+            },
             VrellisShape::Polygon { .. } => unimplemented!(),
+            VrellisShape::Parabola => unimplemented!(),
+            VrellisShape::Custom { points } => {
+                points.clone()
+            }
+
         }
         return out;
     }
